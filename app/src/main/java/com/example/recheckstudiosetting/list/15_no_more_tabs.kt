107488bc691d421365b949c9f13ad8252c04653b
pluulove <pluulove@gmail.com>
2019-06-23 16:22:33 +0900

    Add, Sample

--- app/src/main/java/com/example/recheckstudiosetting/list/15_no_more_tabs.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/15_no_more_tabs.kt
@@ -2,32 +2,3 @@ package com.example.recheckstudiosetting.list
 
 // Preferences | Editor | General | Editor Tabs
 // Appearance | Placement > None
-
-
-
-
-
-// ↓
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ app/src/main/java/com/example/recheckstudiosetting/list/17_logcat_color.kt
@@ -0,0 +1,3 @@
+package com.example.recheckstudiosetting.list
+
+// Preferences | Editor | Color Scheme | Android Logcat
