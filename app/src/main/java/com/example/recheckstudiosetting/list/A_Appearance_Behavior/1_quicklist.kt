37b547804ab75710765964675ca53dcab46e32f9
pluulove <pluulove@gmail.com>
2019-07-10 00:17:42 +0900

    Update, Quick List 기본 Function

--- app/src/main/java/com/example/recheckstudiosetting/list/A_Appearance_Behavior/1_quicklist.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/A_Appearance_Behavior/1_quicklist.kt
@@ -2,3 +2,6 @@ package com.example.recheckstudiosetting.list.A_Appearance_Behavior
 
 // 자신이 원하는 형태의 Popup 생성 가능
 // Preferences | Appearance & Behavior | Quick Lists
+private fun sample() {
+    val value = "pluu"
+}
