cc3a1259057ac1e75f019f6b1a6f679b0d007890
pluulove <pluulove@gmail.com>
2020-07-18 13:05:05 +0900

    Add Sample Case

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/F_Build_Execution_Deployment/21_dependency_breakpoint.kt
@@ -0,0 +1,13 @@
+package com.example.recheckstudiosetting.list.F_Build_Execution_Deployment
+
+// Disable until hitting the following breakpoint
+// Site: https://www.jetbrains.com/help/idea/using-breakpoints.html#properties
+// Breakpoint icons : https://www.jetbrains.com/help/idea/using-breakpoints.html#breakpoint-icons
+
+fun trigger_breakpoint(value: Int) {
+    print(value)
+}
+
+fun dependency_breakpoint(value: Int) {
+    print(value)
+}
---
+++ app/src/main/java/com/example/recheckstudiosetting/list/other/22_logging_fold_lines.kt
@@ -0,0 +1,5 @@
+package com.example.recheckstudiosetting.list.other
+
+// 1. Logcat Dialog Open
+// 2. Right Menu
+// 3. Select "Fold Lines Like This"
