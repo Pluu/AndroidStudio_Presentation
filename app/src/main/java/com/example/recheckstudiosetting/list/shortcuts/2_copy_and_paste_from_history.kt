bbf15bfa4ce4ae58a4991988576055e5b974a333
pluulove <pluulove@gmail.com>
2020-07-22 13:41:35 +0900

    Update Comment

--- app/src/main/java/com/example/recheckstudiosetting/list/shortcuts/2_copy_and_paste_from_history.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/shortcuts/2_copy_and_paste_from_history.kt
@@ -2,3 +2,9 @@ package com.example.recheckstudiosetting.list.shortcuts
 
 // Chose Content to Paste
 // Cmd + Shift + V
+
+// Old
+// File > Settings > Editor > Limits > Maximum number of contents to keep in clipboard
+
+// IntelliJ IDEA 2020.1
+// Opt2. Menu - Help - Find Action - Registry - clipboard.history.max.items
