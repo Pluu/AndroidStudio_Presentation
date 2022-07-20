3a982e0f922fed8cade833025ae2aec7fc5ca7a4
pluu <pluulove@gmail.com>
2022-07-21 00:42:01 +0900

    24_method_separators 추가

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/C_Editor/24_method_separators.kt
@@ -0,0 +1,15 @@
+package com.example.recheckstudiosetting.list.C_Editor
+
+// Preferences | Editor | General | Appearance
+
+private fun t1() {
+
+}
+
+private fun t2() {
+
+}
+
+private fun t3() {
+
+}
