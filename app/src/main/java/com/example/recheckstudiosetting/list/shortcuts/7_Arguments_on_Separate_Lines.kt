d1b6c902030a9b5fa2fb8d36a57852448476a35a
pluu <pluulove@gmail.com>
2022-07-21 23:40:41 +0900

    Add, Arguments on Separate Lines

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/shortcuts/7_Arguments_on_Separate_Lines.kt
@@ -0,0 +1,7 @@
+package com.example.recheckstudiosetting.list.shortcuts
+
+// 1. ⌥⏎ (macOS) or Alt+Enter (Windows/Linux)
+// 2. select 'Put Arguments on Separate Lines'
+// https://twitter.com/intellijidea/status/1523573737584427008?s=20&t=nZTRcugzKU8kWHFglkGi0Q
+
+private class TestSeparateLines(val a: String, val b: Int, val c: String)
