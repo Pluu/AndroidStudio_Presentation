f981593c8a3af47e33d0ad3a1bcc8d526cdc128a
pluu <pluulove@gmail.com>
2023-06-08 02:08:27 +0900

    Update, parameters_on_separate lines

--- app/src/main/java/com/example/recheckstudiosetting/list/other/10_parameters_on_separate lines.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/other/10_parameters_on_separate lines.kt
@@ -1,7 +1,9 @@
-package com.example.recheckstudiosetting.list.shortcuts
+package com.example.recheckstudiosetting.list.other
 
 // 1. ⌥⏎ (macOS) or Alt+Enter (Windows/Linux)
 // 2. select 'Put Arguments on Separate Lines'
 // https://twitter.com/intellijidea/status/1523573737584427008?s=20&t=nZTRcugzKU8kWHFglkGi0Q
 
-private class TestSeparateLines(val a: String, val b: Int, val c: String)
+fun test(value1: String, value2: Int, value3: Float) {
+    // TODO: blabla
+}
