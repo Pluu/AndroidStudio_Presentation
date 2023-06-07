31277a90e6069cf34abb936f66bb1d0e6d6995a9
pluu <pluulove@gmail.com>
2023-06-08 00:49:44 +0900

    Add, Exclude classes and packages from auto import

---
+++ app/src/test/java/com/example/recheckstudiosetting/1_exclude_auto_import.kt
@@ -0,0 +1,12 @@
+package com.example.recheckstudiosetting
+
+// Preferences | Editor | General | Auto Import
+// Exclude classes and packages from auto import
+// https://www.jetbrains.com/help/idea/creating-and-optimizing-imports.html#exclude-from-auto-import
+
+//class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+//}
