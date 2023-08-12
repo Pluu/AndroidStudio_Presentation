4663417212deabb671a07db02c0d29bf87758378
pluu <pluulove@gmail.com>
2023-08-13 01:03:14 +0900

    Add, Switch between schemes

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/shortcuts/13_Theme_Switch.kt
@@ -0,0 +1,6 @@
+package com.example.recheckstudiosetting.list.shortcuts
+
+// Switch between schemes
+// Main Menu | View | Quick Switch Scheme...
+// ^` (macOS)
+// https://www.jetbrains.com/help/objc/switching-between-schemes.html
