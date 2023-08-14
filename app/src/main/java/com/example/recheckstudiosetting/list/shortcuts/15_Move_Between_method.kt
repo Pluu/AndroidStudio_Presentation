05b7ae82c596e0f2a3128652519b0bac28ed04d6
pluu <pluulove@gmail.com>
2023-08-14 16:40:30 +0900

    Add, Move Between method

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/shortcuts/15_Move_Between_method.kt
@@ -0,0 +1,25 @@
+package com.example.recheckstudiosetting.list.shortcuts
+
+// Main Menu | Navigate | Navigate in File | Previous Method
+// Main Menu | Navigate | Navigate in File | Next Method
+
+private fun t1() {
+
+}
+
+private fun t2() {
+
+}
+
+private fun t3() {
+
+}
+
+private fun t4() {
+
+}
+
+private fun t5() {
+
+}
+
