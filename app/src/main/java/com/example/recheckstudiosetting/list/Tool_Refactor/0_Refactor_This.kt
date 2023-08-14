ad228a9a4875e3cf8203ac7aabb5922088cd2ccb
pluu <pluulove@gmail.com>
2023-08-14 21:12:18 +0900

    Add, Refactor This

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/Tool_Refactor/0_Refactor_This.kt
@@ -0,0 +1,8 @@
+package com.example.recheckstudiosetting.list.Tool_Refactor
+
+// Main Menu | Refactor | Refactor This...
+// - ⌃ Ctrl + T
+// https://www.jetbrains.com/help/idea/refactoring-source-code.html
+private fun refactor() {
+    val i = 10
+}
