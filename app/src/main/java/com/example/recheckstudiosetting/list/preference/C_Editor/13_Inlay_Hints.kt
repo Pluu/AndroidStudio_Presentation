e6c846c4df1eafcd2e1682a51cf28ce823d52f43
pluu <pluulove@gmail.com>
2023-08-19 17:11:47 +0900

    Update, Inlay Hints

--- app/src/main/java/com/example/recheckstudiosetting/list/preference/C_Editor/13_Inlay_Hints.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/preference/C_Editor/13_Inlay_Hints.kt
@@ -9,7 +9,11 @@ val a = listOf(1, 2, 3).sum()
 private fun test(a: Int, b: String) {
     // TODO
 }
-
-private fun sample() {
-    com.example.recheckstudiosetting.list.preference.C_Editor.test(10, "20")
+private fun test2() {
+    test(10, "20")
 }
+
+private fun test3() = (0..10)
+    .map {
+        it * 2
+    }.joinToString(separator = ",") { it.toString() }
