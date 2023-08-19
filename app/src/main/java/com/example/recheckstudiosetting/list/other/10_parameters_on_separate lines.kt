f37ba7d680739412970b6935e1dd045232e6cb40
pluu <pluulove@gmail.com>
2023-08-19 12:21:02 +0900

    Update, action name

--- app/src/main/java/com/example/recheckstudiosetting/list/other/10_parameters_on_separate lines.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/other/10_parameters_on_separate lines.kt
@@ -1,7 +1,7 @@
 package com.example.recheckstudiosetting.list.other
 
 // 1. ⌥ Opt + ⏎ Enter (macOS) or Alt+Enter (Windows/Linux)
-// 2. select 'Put Arguments on Separate Lines'
+// 2. select 'Put parameters on separate lines'
 // https://twitter.com/intellijidea/status/1523573737584427008?s=20&t=nZTRcugzKU8kWHFglkGi0Q
 
 fun test(value1: String, value2: Int, value3: Float) {
