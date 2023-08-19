682b2cb065d85148702a2cb33828bb7e7e6e9ca8
pluu <pluulove@gmail.com>
2023-08-19 22:06:10 +0900

    Update, Change Signature

--- app/src/main/java/com/example/recheckstudiosetting/list/Tool_Refactor/1_change_signature.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/Tool_Refactor/1_change_signature.kt
@@ -1,7 +1,11 @@
 package com.example.recheckstudiosetting.list.Tool_Refactor
 
 // Change Signature (⌘ Сmd + F6)
-// Menu - Refactor - Change Signature 
+// Menu - Refactor - Change Signature
+
+private fun test() {
+    changeSignature()
+}
 
 private fun changeSignature() {
     val msg = "Android Studio"
