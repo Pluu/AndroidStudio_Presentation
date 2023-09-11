8f03e40b28068337f485fc6794bb2b34ed9173a3
pluu <pluulove@gmail.com>
2023-09-11 23:27:37 +0900

    Add, Back/Forward Navigate

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/Tool_Navigate/3_Back_Forward.kt
@@ -0,0 +1,7 @@
+package com.example.recheckstudiosetting.list.Tool_Navigate
+
+// Navigate with the caret
+// - To navigate backwards, press : ⌘ Сmd + [
+// - To navigate forward, press : ⌘ Сmd + ]
+// - To navigate to the last edited location, press : ⌘ Сmd + ⇧ Shift + ⌫ Backspace
+// https://www.jetbrains.com/help/idea/navigating-through-the-source-code.html
