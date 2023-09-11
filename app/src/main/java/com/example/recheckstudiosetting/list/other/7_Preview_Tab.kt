6371b6aa7027798b4550f0a2c22e2422f4e37d15
pluu <pluulove@gmail.com>
2023-09-11 23:27:13 +0900

    Preview Tab 설명 추가

--- app/src/main/java/com/example/recheckstudiosetting/list/other/7_Preview_Tab.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/other/7_Preview_Tab.kt
@@ -2,3 +2,4 @@ package com.example.recheckstudiosetting.list.other
 
 // Preferences | Editor | General | Editor Tabs
 // Opening Policy > [✓] Use, Enable preview tab
+// https://www.jetbrains.com/help/idea/using-code-editor.html#preview-tab
