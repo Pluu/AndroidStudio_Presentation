39d862696a39c8ddda512b3107cd1053683f4e53
pluu <pluulove@gmail.com>
2023-09-11 23:27:00 +0900

    Layout Editor, Mode 단축키 추가

--- app/src/main/java/com/example/recheckstudiosetting/list/preference/C_Editor/3_layout_editor.kt
+++ app/src/main/java/com/example/recheckstudiosetting/list/preference/C_Editor/3_layout_editor.kt
@@ -2,3 +2,7 @@ package com.example.recheckstudiosetting.list.preference.C_Editor
 
 // Layout Editor
 // Preferences | Editor | Design Tools
+
+// Mode 전환
+// - ⇧ Shift + ⌃ Ctrl + ◀︎
+// - ⇧ Shift + ⌃ Ctrl + ▶︎
