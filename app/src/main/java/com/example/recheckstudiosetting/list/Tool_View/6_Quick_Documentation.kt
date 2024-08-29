b5057dea86093140f4b3a5db1d5fe388ab2984cb
pluu <pluulove@gmail.com>
2024-08-30 08:36:02 +0900

    Add Documentation

---
+++ app/src/main/java/com/example/recheckstudiosetting/list/Tool_View/6_Quick_Documentation.kt
@@ -0,0 +1,10 @@
+package com.example.recheckstudiosetting.list.Tool_View
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+// Main Menu | View | Quick Documentation
+// F1
+
+private val _sampleData = MutableLiveData<Boolean>()
+val sampleData: LiveData<Boolean> get() = _sampleData
